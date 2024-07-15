c426f8a9723229ebbc8d033f792d8d13472d1cd8
Geison Pizzio <geison.pizzio@outlook.com>
2024-07-15 00:06:06 -0300

    feat: Cadastramento funcional.

--- src/app/pages/profile/profile-create-update/profile-create-update.component.ts
+++ src/app/pages/profile/profile-create-update/profile-create-update.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ProfileService } from 'src/app/services/profile.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile-create-update',
@@ -9,7 +11,7 @@ import { ProfileService } from 'src/app/services/profile.service';
 })
 export class ProfileCreateUpdateComponent {
   
-  constructor(private profileService: ProfileService){}
+  constructor(private profileService: ProfileService, private router: Router){}
 
   profileForm = new FormGroup({
     name: new FormControl('', Validators.required),
@@ -21,8 +23,16 @@ export class ProfileCreateUpdateComponent {
     explevel: new FormControl(0, [Validators.required, Validators.min(0)])
   })
 
-  onSubmit(){
+  onSubmit() {
     const profile = this.profileForm.value;
-    console.log(profile);
+    console.log(profile)
+    this.profileService.save(profile).subscribe(result => {
+      console.log(result)
+      Swal.fire({
+        title: 'Pessoa cadastrada com sucesso!',
+        icon: 'success',
+      })
+      this.router.navigateByUrl('/profile')
+    });
   }
 }
